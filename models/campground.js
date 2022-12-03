fad09e5a3b2a15621da471e7ecb867472f5dffca
Chukwudi <54580449+chuks2324@users.noreply.github.com>
2022-12-03 00:28:28 +0000

    bootstrap inclusion

--- models/campground.js
+++ models/campground.js
@@ -1,9 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: String,
-  title: String,
-  description: String,
-  title: String,
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String
 });
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
