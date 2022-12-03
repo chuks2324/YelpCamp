d578e76256bf094453a3acfbe1d21f322587a142
Chukwudi <54580449+chuks2324@users.noreply.github.com>
2022-12-03 00:23:10 +0000

    express setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const app = express();
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Serving port 3000");
+});
---
+++ models/campground.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+  title: String,
+  title: String,
+  description: String,
+  title: String,
+});
